737c8479e174090abdebc31647a2b6b08a5b35bd
Indrawahyu12 <rafaindra695@gmail.com>
2024-11-06 14:12:37 +0800

    modul 3

---
+++ main.py
@@ -0,0 +1,24 @@
+# casting adalah cara mengubah tipe data variabel
+
+# INTEGER
+print("===INTEGR===")
+data int = 9
+print("data = ", data int, ", type =", type(data_int))
+
+# casting
+data_float = float(data_int)
+data_str = str(data_int)
+dta_bool = bool(data_int)
+pirnt("data = ", data_float, ", type =", type(data_float))
+pirnt("data = ", data_str, ", type =", type(data_str))
+pirnt("data = ", data_bool, ", type =", type(data_bool))
+
+# FLOAT
+print("===FLOAT===")
+data_float = 3.14
+print("data = ", data_float, ", type =", type(data_float))
+
+# casting
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
